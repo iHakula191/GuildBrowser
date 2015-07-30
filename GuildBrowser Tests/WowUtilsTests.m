d944b16cc4bc1aee7fa3d6163de5aadcb900ea1b
wwsun <wwsun@thoughtworks.com>
2015-07-30 16:01:56 +0800

    add tests

---
+++ GuildBrowser Tests/WowUtilsTests.m
@@ -0,0 +1,65 @@
+//
+//  WowUtilsTests.m
+//  GuildBrowser
+//
+//  Created by TWer  on 7/30/15.
+//  Copyright (c) 2015 Charlie Fulton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "WowUtils.h"
+
+@interface WowUtilsTests : XCTestCase
+
+@end
+
+@implementation WowUtilsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+// 1
+-(void)testCharacterClassNameLookup
+{
+    // 2
+    XCTAssertEqualObjects(@"Warrior",
+                         [WoWUtils classFromCharacterType:1],
+                         @"ClassType should be Warrior");
+    // 3
+    XCTAssertFalse([@"Mage" isEqualToString:[WoWUtils classFromCharacterType:2]]);
+    
+    // 4
+    XCTAssertTrue([@"Paladin" isEqualToString:[WoWUtils classFromCharacterType:2]]);
+    // add the rest as an exercise
+}
+
+- (void)testRaceTypeLookup
+{
+    XCTAssertEqualObjects(@"Human", [WoWUtils raceFromRaceType:1]);
+    XCTAssertEqualObjects(@"Orc", [WoWUtils raceFromRaceType:2]);
+    XCTAssertFalse([@"Night Elf" isEqualToString:[WoWUtils raceFromRaceType:45]]);
+    // add the rest as an exercise
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
